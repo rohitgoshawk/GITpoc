110944b17c2190195cd0f01fd971267af087b0b4
Rohit Mishra1 <rohitgoshawk@gmail.com>
2023-09-13 10:30:49 +0200

    chamnged author name

--- src/Test2.java
+++ src/Test2.java
@@ -1,9 +1,10 @@
 
 public class Test2 {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       String a="";
+       String ab="";
 	}
 
 }
