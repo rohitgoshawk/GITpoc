c5ed30cbe95e72e22fdd244c40a61427a2ba4657
Rohit Mishra1 <xploreh2@gmail.com>
2023-09-13 10:33:08 +0200

    dev02

--- src/Test2.java
+++ src/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       String ab="";
+       String avb="";
 	}
 
 }
